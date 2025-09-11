896f394de0ba78248514b25ec4dba5dc6f3cc7c8
gio chili <playgeographic@gmail.com>
2025-09-11 12:21:41 +0400

    545

--- GeographicWorkWebAPI/Program.cs
+++ GeographicWorkWebAPI/Program.cs
@@ -29,6 +29,7 @@ builder.Services.AddTransient<DictionaryDTO>();
 builder.Services.AddTransient<IVarjisFarti, VarjisFartiRepository>();
 builder.Services.AddTransient<IAero, AeroGadagebaRepository>();
 builder.Services.AddTransient<IAeroWithObjectId, AeroGadagebaWithObjectIdRepository>();
+builder.Services.AddTransient<IAeroWithoutFolders, AeroGadagebaWithoutFoldersRepository>();
 builder.Services.AddTransient<IWindbreak, WindbreakRepository>();
 builder.Services.AddTransient<IColumnName, ColumnNameRepository>();
 builder.Services.AddTransient<IChromeBot, ChromeBotRepository>();
